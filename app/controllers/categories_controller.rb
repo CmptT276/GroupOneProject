8c0b1e98ce060a6b9c7d791c8bad709256de7c46
cmpt276groupone <cmpt276groupone@hotmail.com>
2016-03-14 06:43:15 +0000

    updating categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -16,6 +16,11 @@ class CategoriesController < ApplicationController
 
   def show
       @category = Category.find(params[:id])
+      
+      respond_to do |format|
+        format.html # show.html.erb
+        format.json { render json: @category }
+      end
   end
 
   def edit
