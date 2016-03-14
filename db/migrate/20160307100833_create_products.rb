075d8511f968e2b234f6f7aff385499bc26fcf9b
cmpt276groupone <cmpt276groupone@hotmail.com>
2016-03-14 04:33:11 +0000

    fixing categories and unlinking to products for now

--- db/migrate/20160307100833_create_products.rb
+++ db/migrate/20160307100833_create_products.rb
@@ -5,11 +5,9 @@ class CreateProducts < ActiveRecord::Migration
       t.text :description
       t.integer :price
       t.references :user, index: true, foreign_key: true
-      t.reference :categories, index: true, foreign_key: true
 
       t.timestamps null: false
     end
     add_index :products, [:user_id, :created_at]
-    add_index :categories, [:categoty_id, :created_at]
   end
 end
